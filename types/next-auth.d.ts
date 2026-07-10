aec8811a19a282b63a6d9fac6ab596a6e7010977
sangharsh495 <sangharsh495@users.noreply.github.com>
2026-07-10 21:56:33 +0530

    Auto-commit: 2026-07-10 21:56:33

---
+++ types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth"
+import { DefaultJWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+      onboardingComplete?: boolean
+    } & DefaultSession["user"]
+  }
+
+  interface User extends DefaultUser {
+    id: string
+    onboardingComplete?: boolean
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id?: string
+    onboardingComplete?: boolean
+  }
+}
