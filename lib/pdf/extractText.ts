995d81ed44bb2885893bc7d9f35cc37bffe0e035
sangharsh495 <sangharsh495@users.noreply.github.com>
2026-07-12 01:34:51 +0530

    Auto-commit: 2026-07-12 01:34:51

--- lib/pdf/extractText.ts
+++ lib/pdf/extractText.ts
@@ -5,8 +5,8 @@ const dom = new JSDOM();
 global.DOMMatrix = dom.window.DOMMatrix;
 global.Path2D = dom.window.Path2D;
 
+// @ts-expect-error No types for internal import
 import pdfParse from "pdf-parse/lib/pdf-parse.js";
-
 export async function extractPdfText(buffer: Buffer): Promise<{ text: string; numPages: number }> {
   const data = await pdfParse(buffer);
   return { text: data.text, numPages: data.numpages };
